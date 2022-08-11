20cb823420067d0aa1743b06eeea8ab58e79801b
Alf-Anas <43590041+Alf-Anas@users.noreply.github.com>
2022-08-11 22:30:41 +0700

    Add files via upload

---
+++ app.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"database/sql"
+	"simpleapi/handlers"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	  }))
+	db := initDB("storage.db")
+	migrate(db)
+
+	e.GET("/tasks", handlers.GetTasks(db))
+	e.POST("/tasks", handlers.PutTask(db))
+	e.POST("/tasks/edit", handlers.EditTask(db))
+	e.GET("/tasks/:id/delete", handlers.DeleteTask(db))
+
+	e.Logger.Fatal(e.Start(":8000"))
+}
+
+func initDB(filepath string) *sql.DB {
+	db, err := sql.Open("sqlite3", filepath)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if db == nil {
+		panic("db nil")
+	}
+
+	return db
+}
+
+func migrate(db *sql.DB) {
+	sql := `
+    CREATE TABLE IF NOT EXISTS tasks(
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		name VARCHAR NOT NULL,
+		pic VARCHAR NOT NULL,
+		deadline VARCHAR NOT NULL,
+		status INTEGER
+    );
+    `
+
+	_, err := db.Exec(sql)
+	// Exit if something goes wrong with our SQL statement above
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module simpleapi
+
+go 1.16
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.14
+	github.com/stretchr/testify v1.8.0 // indirect
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
+)
